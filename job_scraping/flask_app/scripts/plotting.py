7941f6f7295a8b44432cadc58133e45ab8373f4f
Kakackle <markowskimaciej98@gmail.com>
2023-12-07 18:14:04 +0100

    unfinished work on a more comprehensive plotting function, with stylizations etc

--- job_scraping/flask_app/scripts/plotting.py
+++ job_scraping/flask_app/scripts/plotting.py
@@ -6,6 +6,7 @@ from datetime import datetime, date
 import wordcloud
 from wordcloud import WordCloud, STOPWORDS
 import os
+import re
 
 from .path_traversal import reset_path, find_file
 
@@ -46,11 +47,21 @@ def plot_by_file(file):
 
 # make multiple plots instead of a singular company plot
 # from file name
+# TODO: poprzerzucac moze tworzenie poszczegolnych typow plotow do osobnych funkcji
+# zeby mozna bylo podac dataframei nazwe kolumny 
+# TODO: wyglad - zadne usuwanie whitespace ani nic nie dziala....
 def plot_by_file_complete(file: str):
     base_path = reset_path()
     path = os.path.join(base_path, 'scraping_results')
     df_file = find_file(file, path)
     df = pd.read_csv(df_file, index_col=0)
+
+    date_regex = re.compile(r'(.*)_(\d{4}-\d{2}-\d{2})')
+    regex = date_regex.search(file)
+    terms = regex.group(1)
+    terms = terms.split('_')
+    terms = ', '.join(terms)
+    date_string = regex.group(2)
 
     extra_words = ['']
     stopwords_extra = STOPWORDS.update(['team', 'will', 'looking'])
@@ -76,18 +87,71 @@ def plot_by_file_complete(file: str):
     for column in skills_string.columns:
         sum_string += ' '.join(skills_string[column].str.lower())
     
-    fig, axes = plt.subplots(2,2, figsize=(20,10))
-    fig.suptitle('Multiple explaratory plots img')
+    # fig, axes = plt.subplots(2,2, figsize=(20,10))
+    fig = plt.figure(figsize=(8,16))
+    fig.suptitle(f'Multiple explaratory plots img for [{terms}] - {date_string}')
 
     # plt.figure(figsize=(20,10))
 
-    wc = WordCloud(background_color="white", stopwords=STOPWORDS, max_words=50,
+    # plot skills
+    skills_wc = WordCloud(background_color="white", stopwords=STOPWORDS, max_words=50,
                 width=800, height=400, collocations=False)
-    wordcloud = wc.generate(sum_string)
-    fig.add_subplot(wordcloud)
-    # todo: wykminic to
+    skills_wordcloud = skills_wc.generate(sum_string)
+    skills_ax = fig.add_subplot(3,2,1)
+    skills_ax.imshow(skills_wordcloud)
+    skills_ax.axis('off')
+    skills_ax.set_title("Skills word cloud")
 
+    # skills frequency word pie chart
+    words_fc = pd.Series(skills_wc.words_)
+    words_fc =  np.ceil(words_fc * len(skills_clean))
+    skills_bar_ax = fig.add_subplot(3,2,2)
+    skills_bar_fig = words_fc[:20].plot.pie()
+    # skills_bar_ax.imshow(skills_bar_fig)
+    skills_bar_ax = skills_bar_fig
+    skills_bar_ax.set_title('skills frequency bar chart')
+
+    # analyzing job titles / title wordcloud
+    title_clean = df['title'].dropna()
+    title_string = ' '.join(title_clean.str.lower())
+    wc_title = WordCloud(background_color="white", stopwords=STOPWORDS, max_words=50,
+                        width=800, height=400)
+    title_wordcloud = wc_title.generate(title_string)
+    title_ax = fig.add_subplot(3,2,3)
+    title_ax.imshow(title_wordcloud)
+    title_ax.axis('off')
+    title_ax.set_title("Title word cloud")
+
+    # companies pie
+    companies = df['company'].value_counts()
+    unique_companies = df['company'].unique()
+    companies_ax = fig.add_subplot(3,2,4)
+    companies_plot_fig = companies[:20].plot.pie()
+    companies_ax = companies_plot_fig
+    companies_ax.set_title('companies pie chart')
+
+    # desc word cloud
+    desc_clean = df['description'].dropna()
+    desc_string = ' '.join(desc_clean.str.lower())
+    wc_desc = WordCloud(background_color="white", stopwords=STOPWORDS, max_words=50,
+                        width=800, height=400)
+    desc_wordcloud = wc_desc.generate(desc_string)
+    desc_ax = fig.add_subplot(3,2,5)
+    desc_ax.imshow(desc_wordcloud)
+    desc_ax.axis('off')
+    desc_ax.set_title("Desc word cloud")
+
+    # locations
+    location_clean = df['location'].dropna()
+    location_count_fig = location_clean.value_counts().plot.pie()
+    location_ax = fig.add_subplot(3,2,6)
+    location_ax = location_count_fig
+    location_ax.set_title("Location count")
+    
+    
     today = str(date.today())
     file_name = f'{file}_wc_{today}'
-    fig.get_figure().savefig(f'./flask_app/static/images/plots/{file_name}.png')
+    fig.get_figure().savefig(f'./flask_app/static/images/plots/{file_name}.png',
+                             bbox_inches='tight',
+                             pad_inches=0.0)
     return file_name
